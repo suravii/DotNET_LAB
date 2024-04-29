61c53e2cd0922fb5c7da6adc9dc84897c9007c8d
Suravi Shrestha <87415031+suravii@users.noreply.github.com>
2024-04-29 14:19:10 +0545

    Add files via upload

---
+++ CollegeLAB/AdoCrud/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdoCrud.Models
+{
+    public class Student
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name is required")]
+        [StringLength(60, MinimumLength =2, ErrorMessage ="Name must be 2 to 60 character long.")]
+        public string Name { get; set; }
+
+        public string Stream { get; set; }
+    }
+}
