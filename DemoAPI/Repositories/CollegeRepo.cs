0e32cabc68bc2712a3eb089b31d2039dd8328b25
Suravi Shrestha <87415031+suravii@users.noreply.github.com>
2024-04-29 13:07:59 +0545

    Add files via upload

---
+++ DemoAPI/Repositories/CollegeRepo.cs
@@ -0,0 +1,39 @@
+﻿using DemoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoAPI.Repositories
+{
+    public class CollegeRepo : IRepository<College>
+    {
+        CollegeDbContext _context= null;
+        public CollegeRepo(CollegeDbContext context)
+        {
+            _context= context;            
+        }
+        public void AddRecord(College model)
+        {
+            _context.Add(model);
+            _context.SaveChanges();
+        }
+
+        public College DeleteRecord(College model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<College> GetAllRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public College GetSingleRecord(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public College UpdateRecord(College model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DemoAPI/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoAPI.Repositories
+{
+    public interface IRepository<T>
+    {
+        public void AddRecord(T model);
+        public IEnumerable<T> GetAllRecords();
+        public T GetSingleRecord(int id);
+        public T UpdateRecord(T model);
+        public T DeleteRecord(T model);
+    }
+}
