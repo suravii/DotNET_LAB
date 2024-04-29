2ca8967a5b2f488ab2121346246a31922c772a7d
Suravi Shrestha <87415031+suravii@users.noreply.github.com>
2024-04-29 13:10:01 +0545

    Add files via upload

---
+++ DemoAPI/Models/CollegeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoAPI.Models
+{
+    public class CollegeDbContext:DbContext
+    {
+        public CollegeDbContext(DbContextOptions<CollegeDbContext> options) : base(options) 
+        {
+            
+        }
+        DbSet<College> Colleges { get; set; }
+    }
+}
