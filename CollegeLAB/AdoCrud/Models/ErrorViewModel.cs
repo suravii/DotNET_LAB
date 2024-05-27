d773a2ba7892cd64f80f519a54d6ff1d6af01a36
Suravi Shrestha <87415031+suravii@users.noreply.github.com>
2024-05-27 22:02:11 +0545

    Delete CollegeLAB directory

--- CollegeLAB/AdoCrud/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AdoCrud.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
